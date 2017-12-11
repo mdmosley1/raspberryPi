ae4cc5fd153445239d1ec347ccce38ed27663e35
Matthew Mosley <mdmosley1@gmail.com>
2017-12-11 15:46:38 -0600

    Added function to diffDriveController class to handle waypoints

--- src/robot_control/src/DiffDriveController.py
+++ src/robot_control/src/DiffDriveController.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
 
 import numpy as np
+import pdb
 
 class DiffDriveController():
     """
     Class used for controlling the robot linear and angular velocity
     """
-    def __init__(self, max_speed, max_omega):
+    def __init__(self, max_speed, max_omega, waypoints):
         # TODO for Student: Specify these parameters
-        self.kp=.2
-        self.ka=2
-        self.kb=0
+        self.kp = 0.2
+        self.ka = 2
+        self.kb = 0
         self.tol = .1
         self.MAX_SPEED = max_speed
         self.MAX_OMEGA = max_omega
+        self.done = False
+
+        self.waypointID = 0
+        self.waypoints = waypoints
+        self.goal = waypoints[0,:]
+        self.numberOfWayPoints = waypoints.shape[0]
         
-    def compute_vel(self, state, goal):
+    def compute_vel(self, state):
         """
         Function that computes the desired outputs given the state and goal
         Inputs:
@@ -29,27 +36,43 @@ class DiffDriveController():
         done - a boolean value specifying if the robot has reached its goal (or
             is close enough
         """
-        # YOUR CODE HERE
-        done = False
+        pi = np.pi
+        
         theta = state[2]
-        deltaX = goal[0] - state[0]
-        deltaY = goal[1] - state[1]
+        deltaX = self.goal[0] - state[0]
+        deltaY = self.goal[1] - state[1]
         rho = np.sqrt(deltaX**2 + deltaY**2)
         alpha = -theta + np.arctan2(deltaY,deltaX)
+        if alpha > pi:
+            alpha -= 2*pi
+        elif alpha < -pi:
+            alpha += 2*pi
         beta = -theta - alpha
 
         v = self.kp * rho
         omega = self.ka*alpha + self.kb*beta
 
-        if abs(v) > self.MAX_SPEED:
+        if v > self.MAX_SPEED:
             v = self.MAX_SPEED
 
-        if abs(omega) > self.MAX_OMEGA:
-            omega = self.MAX_OMEGA
+        if v < 0.1:
+            v = 0.1
 
-        # saturate linear and angular velocity 
-        
+        if omega > self.MAX_OMEGA:
+            omega = self.MAX_OMEGA
+        elif omega < -self.MAX_OMEGA:
+            omega = -self.MAX_OMEGA            
+
         # set done to true if robot is close to goal
         if np.abs(rho) < self.tol:
-            done = True
-        return v,omega,done
+            if self.waypointID < self.numberOfWayPoints-1:
+                self.goal = self.getNextWaypoint()
+            else:
+                self.done = True
+
+        return v,omega,self.done
+
+    def getNextWaypoint(self):
+        self.waypointID += 1            
+        currentWaypoint = self.waypoints[self.waypointID,:]
+        return currentWaypoint
